d27b2740c1bedfef42bf0dec50c0ce19e22bc993
Farn <93525542+FarnGitHub@users.noreply.github.com>
2023-04-05 09:59:59 +0700

    2.0 thing

--- net/minecraft/src/mod_FarnAnnoyanceFix.java
+++ net/minecraft/src/mod_FarnAnnoyanceFix.java
@@ -8,55 +8,40 @@ import org.lwjgl.input.Mouse;
 
 public class mod_FarnAnnoyanceFix extends BaseMod {
 
-	@MLProp(name="ModernFencePlacement", info="Make fence placable without require block underneath")
-	public static boolean fencefix = true;
-
-	@MLProp(name="BetterBoat", info="Make boat drop themselves and also make it don't break when collide with something anymore")
-	public static boolean boatfix = true;
-
-	@MLProp(name="CobbleStairDropThemself", info="Make cobblestone stairs drop themselves")
-	public static boolean stonestair = true;
-
-	@MLProp(name="OakDropThemself", info="Make oak stairs drop themselves")
-	public static boolean woodstair = true;
-
-	@MLProp(name="FarnLandNoDirt", info="Remove farm trampling")
-	public static boolean farmland = true;
-
 	public String Version() {
-		return "2.0 Preview 2";
+		return "2.0";
 	}
 
 	public mod_FarnAnnoyanceFix() {
 		ModLoader.SetInGameHook(this, true, false);
 
-		if(boatfix) {
+		if(FarnAnnoyanceFixConfiguration.boatfix) {
 			EntityProxyAPI.getInstance().overrideEntity("Boat", EntityBoat.class, 41, EntityBoatProxy.class);
 			Item.itemsList[Item.boat.shiftedIndex] = null;
 			Item boat = (new ItemBoatProxy(Item.boat.shiftedIndex - 256)).setIconCoord(8, 8).setItemName("boat");
 		}
 
-		if(fencefix) {
+		if(FarnAnnoyanceFixConfiguration.fencefix) {
 			Block.blocksList[85] = null;
 			Block fence = (new BlockFenceProxy(85, 4)).setHardness(2.0F).setResistance(5.0F).setStepSound(Block.soundWoodFootstep).setBlockName("fence").disableNeighborNotifyOnMetadataChange();
 		}
 
-		if(woodstair) {
+		if(FarnAnnoyanceFixConfiguration.woodstair) {
 			Block.blocksList[53] = null;
 			Block stairCompactPlanks = (new BlockStairsProxy(53, Block.planks)).setBlockName("stairsWood").disableNeighborNotifyOnMetadataChange();
 		}
 
-		if(stonestair) {
+		if(FarnAnnoyanceFixConfiguration.stonestair) {
 			Block.blocksList[67] = null;
 			Block stairCompactCobblestone = (new BlockStairsProxy(67, Block.cobblestone)).setBlockName("stairsStone").disableNeighborNotifyOnMetadataChange();
 		}
 		
-		if(farmland) {		
+		if(FarnAnnoyanceFixConfiguration.farmland) {		
 			Block.blocksList[60] = null;
 			Block tilledField = (new BlockFarmlandProxy(60)).setHardness(0.6F).setStepSound(Block.soundGravelFootstep).setBlockName("farmland");
 		}
 
-		ToolEffectiveConfiguration.instance.AddAllToolEffective();
+		FarnAnnoyanceFixConfiguration.instance.AddAllToolEffective();
 	}
 
 	public boolean OnTickInGame(Minecraft game) {
